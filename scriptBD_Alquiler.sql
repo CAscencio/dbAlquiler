b69629d554a71c1814c86a54f9518e5b2ae8d0eb
zecarlos <zsalcedo@vallegraande.edu.pe>
2018-11-26 09:38:02 -0500

    HE creado la tabla persona

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -3,4 +3,32 @@ show databases;
 /*Elimar la base de datos Alquiler*/
 drop database dbAlquiler;
 
-/**/
+/*Crear nuestra BD Alquiler teniendo en cuenta la  configuracion de caracteres*/
+CREATE DATABASE dbAlquiler DEFAULT CHARACTER SET utf8;
+/*Poner en uso la BD*/
+use dnAlquiler;
+/*Verificar que la bd este en uso*/
+select database();
+/*Creamos una tabla*/
+CREATE TABLE PERSONA
+(
+    CODPER int,
+    NOMPER varchar(100),
+    APEPATPER varchar(100),
+    APEMATPER varchar (100),
+    DNIPER char (8),
+    DIRPER varchar (150),
+    UBIGEO_CODUBI  char(6),
+    TELEF1PER char(9),
+    TELEF2PER char(9),
+    SEXOPER char(1),
+    TIPOPER char (1),
+    USEPER varchar (50),
+    PASSPER varchar (50),
+    CONSTRAINT CODPER_PK PRIMARY KEY (CODPER)
+);
+/*Mostrar tablas*/
+show tables;
+
+/*Mostar sus campos*/
+SHOW COLUMNS IN PERSONA;
